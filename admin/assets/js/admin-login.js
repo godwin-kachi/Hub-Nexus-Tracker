833845077b911d622439dc148f8b79f83c8f7fe0
Godwin <godwinkachi@hotmail.com>
2024-07-23 18:18:39 +0100

    Completing the interface design for the front facing app.

--- admin/assets/js/admin-login.js
+++ admin/assets/js/admin-login.js
@@ -5,6 +5,20 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const username = form.username.value;
-  const password = form.password.value;
+  const logCredentials = {
+    username: form.username.value,
+    password: form.password.value,
+  };
+
+  const userData = {
+    method: "POST",
+    cors: "no-cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(logCredentials),
+  };
+
+  // Write the fetch request below
 });
