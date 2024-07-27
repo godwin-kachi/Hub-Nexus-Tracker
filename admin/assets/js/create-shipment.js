56d2090e48477e3bd42bac73877b5b4fe314238f
profinnocent <innocent.unachukwu@gmail.com>
2024-07-27 11:43:15 +0100

    woked on create-shipment files

--- admin/assets/js/create-shipment.js
+++ admin/assets/js/create-shipment.js
@@ -1,5 +1,4 @@
 const createShipmentForm = document.getElementById("create-shipment-form");
-console.log(createShipmentForm);
 
 createShipmentForm.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -18,25 +17,26 @@ createShipmentForm.addEventListener("submit", async (event) => {
     receiver_phone: formData.get("receiver_phone"),
     receiver_address: formData.get("receiver_address"),
     sending_loc: formData.get("sending_loc"),
-    delivery_loc: formData.get("del_loc"),
+    delivery_loc: formData.get("delivery_loc"),
     service_price: formData.get("shipping_cost"),
-    delivery_type: parseFloat(formData.get("del_type")),
+    delivery_type: parseFloat(formData.get("delivery_type")),
+    delivery_price: formData.get("delivery_price"),
     comment: formData.get("comment"),
   };
-  console.log(formData);
-  console.log(shipmentData);
 
   const apiurl = `${location.protocol}//${location.hostname}/api`;
 
   // try {
   //   const response = await fetch(`${apiurl}/packageapi/createpackage`, {
   //     method: 'POST',
+  //     mode: 'no-cors',
   //     headers: {
-  //       'Content-Type': 'application/json'
+  //       "Content-Type": "application/json",
+  //       "Accept": "application/json"
   //     },
   //     body: JSON.stringify(shipmentData)
   //   });
-
+  //
   //   if (response.ok) {
 
   //     alert('Shipment created successfully!');
@@ -49,3 +49,56 @@ createShipmentForm.addEventListener("submit", async (event) => {
   //   alert('An error occurred while creating the shipment. Please try again later.');
   // }
 });
+
+
+
+
+
+
+// Manage total cost calculation
+
+const shippingCostInput = document.getElementById("shipping_cost");
+const deliveryTypeInput = document.getElementById("del_type");
+const deliveryCostInput = document.getElementById("delivery_cost");
+const delCostDiv = document.getElementById("del_cost_div");
+const totalCostElement = document.getElementById("total_cost");
+
+
+
+shippingCostInput.addEventListener("change", calculateTotalCost);
+shippingCostInput.addEventListener("focus", () => {
+  shippingCostInput.value = ""
+});
+
+
+
+deliveryCostInput.addEventListener("change", calculateTotalCost);
+deliveryCostInput.addEventListener("focus", () => {
+  deliveryCostInput.value = ""
+});
+
+function calculateTotalCost() {
+   tcost = parseFloat(shippingCostInput.value) + parseFloat(deliveryCostInput.value);
+  console.log(tcost);
+  console.log(new Intl.NumberFormat().format(tcost))
+  
+  totalCostElement.value = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'NGN' }).format(tcost)
+  // totalCostElement.value = "N" + tcost.toFixed(2)
+
+}
+
+
+deliveryTypeInput.addEventListener('change', showDeliveryCostInput);
+
+function showDeliveryCostInput() {
+
+  if (deliveryTypeInput.value == 0) {
+    delCostDiv.style.display = 'none';
+    deliveryCostInput.value = 0;
+    calculateTotalCost(); 
+
+  }
+  else {
+    delCostDiv.style.display = 'block';
+  }
+}
